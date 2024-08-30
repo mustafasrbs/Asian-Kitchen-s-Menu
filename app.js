706110161faa4c7d661e7540bef1c984cdda91bb
Mustafa <mustafasrbs1@gmail.com>
2024-08-30 17:57:07 +0300

    data returned to html and showed

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-fetch('./menu.json')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok ' + response.statusText);
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log('Fetched Data:', data);
-    // JSON verisini işlemek için buraya kod ekleyin
-  })
-  .catch(error => {
-    console.error('Fetch operation failed:', error);
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('./menu.json')
+      .then(response => response.json())
+      .then(data => {
+        const menu = data;
+        displayMenuItems(menu); // Menü öğelerini sayfada gösteriyoruz
+        displayMenuButtons(menu); // Filtre butonlarını dinamik olarak oluşturuyoruz
+      })
+      .catch(error => console.error('Fetch operation failed:', error));
   });
+  
+  function displayMenuItems(menuItems) {
+    const sectionCenter = document.querySelector('.section-center');
+    
+    let displayMenu = menuItems.map(item => {
+      return `
+        <div class="menu-items col-lg-6 col-sm-12">
+          <img src="${item.img}" class="photo" alt="${item.title}">
+          <div class="menu-info">
+            <div class="menu-title">
+              <h4>${item.title}</h4>
+              <h4 class="price">$${item.price}</h4>
+            </div>
+            <div class="menu-text">
+              ${item.desc}
+            </div>
+          </div>
+        </div> 
+      `;
+    });
+    sectionCenter.innerHTML = displayMenu.join('');
+  }
+  
+  
